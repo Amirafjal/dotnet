eb88744b5479ba6af4399d59ee508c9104875c1b
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2022-01-21 03:35:22 +0530

    project start

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -6,7 +6,14 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            //string name = "amir afjal";
+            //int age = 22;
+            //Console.WriteLine(name+" is "+age );
+
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "amir afjal";
+            Console.WriteLine("{0}: Version {1} by {2}",appName,appVersion,appAuthor);
         }
     }
 }
